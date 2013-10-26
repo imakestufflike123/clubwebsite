fa292248476b64a66e646870c4405f6bd79e3984
Maker <maker@joel-duffins-macbook.local>
2013-10-25 20:08:38 -0600

    initial commit

---
+++ web.rb
@@ -0,0 +1,9 @@
+require 'sinatra'
+
+get '/' do
+  "<h1>Hello, world, this is the beginning of our cache maker app</h1>" +
+  "<p>Hi Howdy</p>" + 
+  '<iframe allowtransparency="true" width="485" height="402" src="http://scratch.mit.edu/projects/embed/13614820/" frameborder="0" allowfullscreen></iframe>' +
+  '<iframe allowtransparency="true" width="485" height="402" src="http://scratch.mit.edu/projects/embed/13614858/" frameborder="0" allowfullscreen></iframe>'
+    
+end
