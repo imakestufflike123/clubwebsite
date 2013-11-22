fe7c24f4d9f50d3ea8e339226ee7ec05afb548d3
Joel Duffin <joel.duffin@gmail.com>
2013-11-21 20:48:30 -0700

    Added agendas page

--- web.rb
+++ web.rb
@@ -1,9 +1,15 @@
 require 'sinatra'
 
+puts "running web server"
+
+set :raise_errors, true
+set :dump_errors, true
+
 get '/' do
   "<h1>Hello, world, this is the beginning of our cache maker app</h1>" +
   "<p>Hi Howdy</p>" + 
   '<iframe allowtransparency="true" width="485" height="402" src="http://scratch.mit.edu/projects/embed/13614820/" frameborder="0" allowfullscreen></iframe>' +
-  '<iframe allowtransparency="true" width="485" height="402" src="http://scratch.mit.edu/projects/embed/13614858/" frameborder="0" allowfullscreen></iframe>'
+  '<iframe allowtransparency="true" width="485" height="402" src="http://scratch.mit.edu/projects/embed/13614858/" frameborder="0" allowfullscreen></iframe>' +
+  '<br/><a href="agendas.html">Agendas</a>'
     
 end
